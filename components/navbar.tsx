09c4c118335d971554953bdf5117de712db0a3ba
marty-m <martynas.miliauskas0@gmail.com>
2023-07-06 00:53:50 +0200

    Add dynamic account dropdown

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+import AccountDropdown from "./account_dropdown";
+
 export default function Navbar() {
     return(
 
@@ -15,7 +17,7 @@ export default function Navbar() {
   <a href="/">
     <div className="avatar">
       <div className="w-20">
-        <img src="utcplusoneblack.svg" alt="" />
+        <img src="/utcplusoneblack.svg" alt="company logo" />
       </div>
     </div>
     </a>
@@ -25,9 +27,8 @@ export default function Navbar() {
     <button className="btn btn-link btn-square">
     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="1" strokeLinecap="square" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
     </button>
-    <button className="btn btn-link btn-square">
-    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="1" strokeLinecap="square" strokeLinejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
-    </button>
+
+    {AccountDropdown()}
     <div className="flex-none">
     <div className="dropdown dropdown-end">
       <label tabIndex={0} className="btn btn-link btn-square">
