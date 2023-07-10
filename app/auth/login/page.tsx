b29fa4ae1f9bf5c4bd8b6546b9b6942171b53df2
marty-m <martynas.miliauskas0@gmail.com>
2023-07-10 18:06:20 +0200

    Render login component

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import Auth from "../auth";
+
+export default function LoginPage() {
+
+    
+    return(
+        <>
+            
+            <Auth />
+            
+            
+        
+        </>
+
+
+
+      )
+}
