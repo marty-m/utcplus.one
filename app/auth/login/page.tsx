f9c45fd611da65b8c2a655a93c3299bcd8437ab4
marty-m <m.miliauskas2@ncl.ac.uk>
2023-07-28 14:26:02 +0200

    Style login form

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -18,30 +18,30 @@ export default function Auth(){
         await login(data);
         reset();
     }
-    
-     
-
-    if (isLoggedIn) return (
-    <>
-    <h1>Logged in: {pb.authStore.model.email}</h1>
-    <button onClick={logout}>Logout</button>
-    </>
-    )
 
     return(
+    <div className="flex flex-col justify-center items-center w-screen">
         
-        <div>
-            <h1>Logged in: {pb.authStore.isValid.toString()}</h1>
-            {isLoading && <p>Loading...</p>}
+        <div className="flex h-3/5 w-1/2 items-center justify-center py-20">
+        
+        <div className="border border-black shadow-xl py-14 px-20">
+        <h1 className=" text-left font-mono font-bold pb-8 text-4xl underline underline-offset-8">SIGN IN</h1>
 
+            <form onSubmit={handleSubmit(onSubmit)}  className="flex flex-col justify-center w-auto">
+                <label className="font-mono text-sm pb-1">EMAIL ADDRESS</label>
+                <input type="email" placeholder="" {...register("email")} />
+                
+                <label className="font-mono text-sm pb-1 pt-4">PASSWORD 
+                <label className="justify-right text-gray-500 font-mono text-right text-xs pl-52"><a href="">FORGOT PASSWORD?</a></label>
+                </label>
+                <input type="password" placeholder="" {...register("password")}/>
 
-
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="email" placeholder="email" {...register("email")} />
-                <input type="password" placeholder="password" {...register("password")}/>
-
-                <button type="submit" disabled={isLoading}>{isLoading? "Loading" : "Login"}</button>
+                <div className="pt-5 pb-4 mx-auto w-full">
+                    <button className="w-full rounded-none bg-yellow-300 py-3 font-mono text-sm" type="submit" disabled={isLoading}>{isLoading? "LOADING" : "SIGN IN"}</button>
+                </div>
             </form>
         </div>
+        </div>
+    </div>
     )
 }
