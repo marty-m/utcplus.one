0dc8af1194e9f7284150be6591469955f7e4adb9
marty-m <m.miliauskas2@ncl.ac.uk>
2023-07-28 13:23:50 +0200

    Restructure root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,11 +21,23 @@ export default function RootLayout({
     <html>
       <body>
         <main>
-          
-            <Navbar />
-              {children}
-            <Footer />
-          
+            <div className="drawer">
+              <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+              <div className="drawer-content h-screen w-screen flex">
+                <Navbar/>
+                  {children}
+              </div> 
+              <div className="drawer-side">
+                <label htmlFor="my-drawer" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                  {/* Sidebar content here */}
+                  <li><a>Sidebar Item 1</a></li>
+                  <li><a>Sidebar Item 2</a></li>
+                  
+                </ul>
+              </div>
+            </div>          
+                       
         </main>
       </body>
     </html>
