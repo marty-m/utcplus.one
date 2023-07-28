8cf137d30db29653e7383ad8f230642f239c5b36
marty-m <m.miliauskas2@ncl.ac.uk>
2023-07-28 13:41:52 +0200

    Restrcture navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,10 +3,9 @@ import AccountDropdown from "./account_dropdown";
 export default function Navbar() {
     return(
 
-<div className="drawer" tabIndex={1}>
-  <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content">
-  <div className="navbar bg-base-000">
+
+  
+  <div className="navbar bg-white fixed top-0 border border-b-black" tabIndex={0}>
   <div className="navbar-start">
   <label htmlFor="my-drawer" className="btn btn-link drawer-button ">
   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="1" strokeLinecap="square" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
@@ -48,18 +47,6 @@ export default function Navbar() {
       </div>
     </div>
   </div>
-  </div>
-</div>
-    
-  </div> 
-  <div className="drawer-side" >
-    <label htmlFor="my-drawer" className="drawer-overlay"></label>
-    <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-      {/* Sidebar content here */}
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-      
-    </ul>
   </div>
 </div>
       
