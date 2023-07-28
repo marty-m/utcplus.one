417ffa7681c85f932deab5b18b791e666c019bc2
marty-m <m.miliauskas2@ncl.ac.uk>
2023-07-28 13:42:07 +0200

    Style sign-up form

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -23,20 +23,51 @@ export default function SignupPage() {
 
 
     return(
-        
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col justify-center items-center w-screen">
+            
+            <div className="flex h-3/5 w-1/2 items-center justify-center py-20">
+            
+            <div className="border border-gray-100 shadow-lg py-14 px-20">
+            <h1 className=" text-left font-mono font-bold pb-8 text-4xl underline underline-offset-8">SIGN UP</h1>
+            
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center w-88">
+                
+                <label className="font-mono text-sm pb-1">EMAIL ADDRESS</label>
+                <input type="email" placeholder="" {...register("email")} required/>
+                
+                <label className="font-mono text-sm pb-1 pt-4">FIRST NAME</label>
+                <input type="text" placeholder="" {...register("fname")} required/>
 
-                <input type="email" placeholder="email" {...register("email")} required/>
-                <input type="text" placeholder="First Name" {...register("fname")} required/>
-                <input type="text" placeholder="Last Name" {...register("lname")} required/>
-                <input type="password" placeholder="password" {...register("password")} required minLength={8}/>
-                <input type="password" placeholder="confirm password" {...register("passwordConfirm")} required minLength={8}/>
+                <label className="font-mono text-sm pb-1 pt-4">LAST NAME</label>
+                <input type="text" placeholder="" {...register("lname")} required/>
 
-
-                <button type="submit" disabled={isLoading}>{isLoading? "Loading" : "Create Account"}</button>
+                <label className="font-mono text-sm pb-1 pt-4">PASSWORD 
+                <label className="justify-right text-gray-500 font-mono text-right text-xs pl-44">(min. 8 characters)</label>
+                </label>
+              
+                <input type="password" placeholder="" {...register("password")} required minLength={8}/>
+                
+                <label className="font-mono text-sm pb-1 pt-4">CONFIRM PASSWORD</label>
+                <input type="password" placeholder="" {...register("passwordConfirm")} required minLength={8}/>
+                
+                <div className="pt-5 pb-4 mx-auto w-full">
+                    <button className="w-full rounded-none bg-yellow-300 py-3 font-mono text-sm" type="submit" disabled={isLoading}>{isLoading? "LOADING" : "CREATE ACCOUNT"}</button>
+                </div>
+                
+                
+                <label className="text-xxs text-gray-500 font-mono">
+                    <input className="checkbox checkbox-xs border border-gray-500 rounded-none focus:ring-transparent" type="checkbox" required></input>
+                    <label className="pl-2">I agree to the <a className="underline" href="">terms and conditions</a> and <a className="underline" href="">privacy policy</a>.</label>
+                    </label>
+                <label className="text-xxs text-gray-500 font-mono pt-2.5">
+                    <input className="checkbox checkbox-xs border border-gray-500 rounded-none focus:ring-transparent" type="checkbox"></input>
+                    <label className="pl-2">Sign me up to the mailing list</label>
+                    </label>    
             </form>
+            </div>
+            </div>
         </div>
+        
     )
 
 
