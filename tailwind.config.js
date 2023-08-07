9d93b77616410e7f4f131157b785e049a171bec1
marty-m <martynas.miliauskas0@gmail.com>
2023-08-07 23:08:21 +0200

    Extend tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
           "--rounded-btn": "0.1rem",
           "--rounded-box": "0.1rem",
           "neutral": "#000000",
-          "accent": "#FDE047"
+          "accent": "#FDE047",
         },
       },
     ],
@@ -20,9 +20,16 @@ module.exports = {
   ],
   theme: {
     extend:{
+      spacing:{
+        '128': '46.8rem',
+      },
       fontSize: {
         xxs: '0.69rem'
-      }}
+      },
+      colors: {
+        palletteBlue: "#113D55",
+    }
+    }
       },
   plugins: [
     require('@tailwindcss/typography'),
