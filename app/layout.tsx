3c0e4febe66eb880b07046198b64fb77963c55b0
marty-m <martynas.miliauskas0@gmail.com>
2023-08-13 22:26:27 +0200

    Remove Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Navbar from '@/app/_components/navbar'
-
-const inter = Inter({ subsets: ['latin'] })
 
 
 export const metadata = {
