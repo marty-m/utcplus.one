d0f1b923199a95a166015fc1a838c5c35b0e466a
marty-m <martynas.miliauskas0@gmail.com>
2023-08-13 22:22:20 +0200

    Update Square catalog hook

--- app/_hooks/useCatalog.tsx
+++ app/_hooks/useCatalog.tsx
@@ -1,16 +1,30 @@
 import { client } from "@/app/square_client";
 
+export const dynamic = 'auto',
+    dynamicparams = true, 
+    revalidate = 0,
+    fetchCache = 'auto',
+    runtime = 'nodejs',
+    preferredRegion = 'auto'
+
 export default function useCatalog(){
+    
     
     async function getAllCatalogObjIDs(){
         try {
-          const response = await client.catalogApi.listCatalog();
+          const response = await client.catalogApi.searchCatalogItems({
+            sortOrder: 'DESC',
+            productTypes: [
+              'REGULAR'
+            ]
+          });
           var objectIDs: string[] = [];
-          response.result.objects.forEach((object: any) => {
-            objectIDs.push(object.id);
+          response.result.items.forEach((object: any) => {
+            if (object.type == "ITEM"){
+              objectIDs.push(object.id);
+            }
           });
           
-          console.log(response.result);
           return objectIDs;
           }
         catch(error) {
@@ -21,16 +35,18 @@ export default function useCatalog(){
     async function getDetails(id: string){
         try {
             const response = await client.catalogApi.retrieveCatalogObject(id);
-            const imageResponse = await client.catalogApi.retrieveCatalogObject(response.result.object.itemData.imageIds[0]);
+            const image1Response = await client.catalogApi.retrieveCatalogObject(response.result.object.itemData.imageIds[0]);
+            const image2Response = await client.catalogApi.retrieveCatalogObject(response.result.object.itemData.imageIds[1]);
           
-            const imageURL: string = imageResponse.result.object.imageData.url;
+            const image1URL: string = image1Response.result.object.imageData.url;
+            const image2URL: string = image2Response.result.object.imageData.url;
            
-
+            const prodID = response.result.object.id;
             const name = response.result.object.itemData.name;
             const description = response.result.object.itemData.description;
             const price = Number(response.result.object.itemData.variations[0].itemVariationData.priceMoney.amount)/100;
 
-            return {name, description, price, imageURL};
+            return {prodID, name, description, price, image1URL, image2URL};
 
           } catch(error) {
             console.log(error);
