66676b8ed1a2dc9414b6288f8a8fd450c5e9077a
marty-m <martynas.miliauskas0@gmail.com>
2023-08-13 22:20:48 +0200

    Style sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,13 +30,34 @@ export default function RootLayout({
                   
               </div> 
               <div className="drawer-side">
-                <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-                  {/* Sidebar content here */}
-                  <li><a>Sidebar Item 1</a></li>
-                  <li><a>Sidebar Item 2</a></li>
+                <div className="w-1/5 h-full bg-stone-950 text-white bg-opacity-90 backdrop-blur-sm">
+                                    
+                  <label htmlFor="my-drawer" className="btn btn-link drawer-button float-right py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="1" strokeLinecap="square" strokeLinejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                  </label>
                   
-                </ul>
+                  <div className='h-1/6'></div>
+                  <div className='px-8 text-gray-200 grid grid-cols-1 place-content-between h-5/6'>
+                    <div className='text-xl grid grid-cols-1 h-max gap-7'>
+                    <a href='/products'>SHOP</a>
+                    <a href='/products/reworked'>REWORKED</a>
+                    <a href='/products/collections'>COLLECTIONS</a>
+                    {/*<a href='/products/lookbooks/ss23'>SS23 LOOKBOOK</a>*/}
+                    <a href='/news'>NEWS</a>
+                    <a href='/about'>ABOUT</a>
+                    </div>
+                    <div className='text-sm grid grid-cols-1 h-max gap-1 pb-6'>
+                      <a href='/support'>SUPPORT</a>
+                      <a href='/terms-and-conditions'>ORDERS T&C</a>
+                      <a href='https://www.instagram.com/utc.one/'>INSTAGRAM</a>
+                      <a href='https://www.tiktok.com/@utc.one'>TIKTOK</a>
+                      <br/>
+                      <p>© 2023 UTC+1 CLOTHING</p>
+                    </div>
+                  </div>
+                  
+                  
+                </div>
               </div>
             </div>          
                        
