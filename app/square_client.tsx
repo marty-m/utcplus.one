cefb6545d8daae68f553e2ed054936468ce9ccaf
marty-m <m.miliauskas2@ncl.ac.uk>
2023-08-17 10:31:56 +0200

    Use production key
    Production key needs to used as Square's sandbox dashboard does not
    support the Square Online interface. This interface is crucial as it
    allows for multiple item images, and other more robust catalog features.

--- app/square_client.tsx
+++ app/square_client.tsx
@@ -2,5 +2,5 @@ const { Client, Environment} = require("square");
 
 export const client = new Client({
   accessToken: process.env.SQUARE_ACCESS_TOKEN,
-  environment: Environment.Sandbox,
+  environment: Environment.Production,
 });
