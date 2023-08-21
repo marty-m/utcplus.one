e66731c27b8f138e21cbf0ca730732ef92091f67
marty-m <martynas.miliauskas0@gmail.com>
2023-08-22 00:08:02 +0200

    Preliminary work
    Component goes into a loop due to async data fetching in a client
    component. Need to fix structuring accordingly to avoid issue.

--- app/products/[id]/variation_form.tsx
+++ app/products/[id]/variation_form.tsx
@@ -1,44 +1,71 @@
-"use client"
-import { getAllItemVariationOptions, getChosenVariationId} from "@/app/_hooks/useCatalog";
+'use client'
+import useCatalog from "@/app/_hooks/useCatalog";
 import { isInStock } from "@/app/_hooks/useInventory";
-import { useState } from "react";
+import { use, useState } from "react";
 
 interface VariationFormProps {
     itemId: string;
 }
 
-export default async function VariationForm({itemId}: VariationFormProps){
-    
-    const allOptions = await getAllItemVariationOptions(itemId);
-    
-    const [variationInStock, setVariationInStock] = useState(false);
-    const []
 
-    async function handleChange(event: any){
+export default function VariationForm({itemId}: VariationFormProps){
+    const { getAllColorSizeVariations, getChosenVariationId } = useCatalog();
 
+    const [chosenColor,setChosenColor] = useState("Gray");  
+    const [chosenSize,setChosenSize] = useState("S");
+
+    
+    const option = getAllColorSizeVariations(itemId);
+    
+    async function checkStock(color:string, size:string){
+        const variationId = await getChosenVariationId(itemId, [color, size]);
+        const inStock = await isInStock(variationId);
+        return inStock;
     }
+    //const [variationInStock, setVariationInStock] = useState(false);
+    
+    
 
     return(
             <form className="flex flex-col items-start">
-            
-                {allOptions?.map((option: any) => {
-                    return (
-                        <>
-                        <label className="text-xl font-bold">{option.name}</label>
-                        <select>
-                            {option.values.map((value: string) => {
-                                
-                                return (
-                                    <option value={value}>{value}</option>
-                                )
-                            })}
-                        </select>
-                        </>
-                    )
-                })}
+
+                <label className="text-xl font-bold">COLOR</label>
+                <select value={chosenColor} onChange={(e) => {
+                    setChosenColor(e.target.value);
+                    }}>
+
+                    {use(option)?.colors.map((color: any) => {
+                        return (
+                            
+                            
+                            <option key={color} value={color}>{color}</option>
+                
+
+                        )
+                    })}
+                </select>
+
+                <label className="text-xl font-bold">SIZE</label>
+                <select value={chosenSize} onChange={(e) => {
+                    setChosenSize(e.target.value);
+                    }}>
+
+                    {use(option)?.sizes.map((size: any) => {
+                        return (  
+
+                        <option key={size} value={size} disabled={use(checkStock(chosenColor,size))? false : true}>
+                            {use(checkStock(chosenColor,size))? size : size + " SOLD OUT"}
+                        </option>
+                        
+                            )
+                    })}
+                </select>
+
+                <p>{chosenColor}</p>
+                <p>{chosenSize}</p>
                            
         
-            <button type="submit" disabled={!variationInStock}>{variationInStock? "ADD TO CART" : "SOLD OUT"}</button>
+            <button disabled={false} onClick={(e) => {console.log("logged")}}>{true? "ADD TO CART" : "SOLD OUT"}</button>
 
             </form>
     )
