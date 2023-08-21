fab26146f30a5a8b271117232e817796ada7d4ac
marty-m <m.miliauskas2@ncl.ac.uk>
2023-08-21 14:12:46 +0200

    Update import for cart dropdown

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,6 +1,9 @@
 import AccountDropdown from "./account_dropdown";
+import CartDropdown from "./cart_dropdown";
 
 export default function Navbar() {
+    
+
     return(
 
 
@@ -28,25 +31,7 @@ export default function Navbar() {
     </button>
 
     <AccountDropdown/>
-    <div className="flex-none">
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-link btn-square">
-        <div className="indicator">
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="1" strokeLinecap="square" strokeLinejoin="round"><circle cx="10" cy="20.5" r="1"/><circle cx="18" cy="20.5" r="1"/><path d="M2.5 2.5h3l2.7 12.4a2 2 0 0 0 2 1.6h7.7a2 2 0 0 0 2-1.6l1.6-8.4H7.1"/></svg>
-          <span className="badge badge-sm indicator-item">8</span>
-        </div>
-      </label>
-      <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-        <div className="card-body">
-          <span className="font-bold text-lg">8 Items</span>
-          <span className="text-info">Subtotal: $999</span>
-          <div className="card-actions">
-            <button className="btn btn-accent btn-block">View cart</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    <CartDropdown/>
   </div>
 </div>
       
