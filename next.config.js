8f832cf73589f207f95aeca3bf01d20acb74b8d4
marty-m <m.miliauskas2@ncl.ac.uk>
2023-08-27 14:45:18 +0300

    Add S3 Bucket hostname

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'items-images-production.s3.us-west-2.amazonaws.com',
+                port: '',
+                pathname: '/files/**',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
