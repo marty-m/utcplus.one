d911b763b89c2f77efce2e51321750a4c64dd0de
marty-m <m.miliauskas2@ncl.ac.uk>
2023-08-29 15:20:07 +0300

    Add persistence to store

--- lib/cartStore.ts
+++ lib/cartStore.ts
@@ -5,21 +5,22 @@ import { CartItem } from '@/lib/types'
 
 interface Store{
     items: CartItem[]
-    variationId: string
     addItem: (item: CartItem) => void
     removeItem: (prodId: string) => void
     totalPrice: () => number
 }
 
-const useCartStore = create<Store>((set: StoreApi<Store>['setState'], get: StoreApi<Store>['getState']) => ({
-        items: [],
-    variationId: "",
-    addItem: (item: CartItem) => set((state) => ({ items: [...state.items, item] })),
-    removeItem: (prodId: string) =>
+const useCartStore = create<Store>()(
+  devtools(persist(
+    (set: StoreApi<Store>['setState'], get: StoreApi<Store>['getState']) => ({
+      items: [],
+      addItem: (item: CartItem) => set((state) => ({ items: [...state.items, item] })),
+      removeItem: (prodId: string) =>
       set((state) => ({ items: state.items.filter((item) => item.itemId !== prodId) })),
-    totalPrice: () =>
-      get().items.reduce((total, item) => total + item.price, 0)
-   
-}))
+      totalPrice: () =>
+        get().items.reduce((total, item) => total + item.price, 0)
+     
+  }), { name: 'cart-storage' }
+  )))
 
 export default useCartStore;
