aa90ac727ef38aaf4533ae84f5c1b94182abc764
marty-m <m.miliauskas2@ncl.ac.uk>
2023-08-30 02:20:41 +0300

    Add additional fields for types

--- lib/types.ts
+++ lib/types.ts
@@ -2,14 +2,18 @@ export type Variations = {
     [colorKey: string]: {
       size: string,
       variation_id: string,
-      inStock: boolean
+      inStock: boolean,
+      imageURLs: string[],
     }[]
   }
 
-  export type CartItem = {
-    itemId: string, 
+  export type CartItem = { 
+    productId: string,
     variationId: string,
+    color: string,
+    size: string,
     images: string[],
     name: string,
     price: number,
+    quantity: number,
 }
