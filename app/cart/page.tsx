5dca0763ba8ddd0c345c5cec719cf29f2e639239
marty-m <m.miliauskas2@ncl.ac.uk>
2023-08-31 18:19:42 +0300

    Add redirect to checkout

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -9,15 +9,18 @@ import {
     TableRow,
   } from "@/components/ui/table"
 import CartItemRow from "@/app/cart/cart_item_row"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function CartPage() {
     const items = useCartStore((state) => state.items)
+    const totalPrice = useCartStore((state) => state.totalPrice)
     return (
         <div className="flex flex-col h-full w-full px-8 py-8">
             <h1 className=" text-2xl font-bold ">YOUR CART</h1>
             <h2 className="text-xs font-serif font-thin pb-5">(at this moment.)</h2>
             <Table>
-                <TableCaption>{"cool cart :)"}</TableCaption>
+                <TableCaption>{items.length === 0 ? "looks a bit empty in here..." : ""}</TableCaption>
                 <TableHeader>
                     <TableRow>
                         <TableHead className="">ITEM</TableHead>
@@ -34,7 +37,14 @@ export default function CartPage() {
                     }): null}
                     
                 </TableBody>
+                
             </Table>
+            <div className="text-right">
+                <p className="">TOTAL: {totalPrice()}</p>
+                <Button asChild variant={"outline"} size={"lg"} className={items.length === 0? "hidden" :"text-lg font-medium py-7 rounded-none border-none bg-yellow-300 hover:bg-yellow-400"}>
+                    <Link href="/cart/checkout"> CHECK OUTTTTT</Link>
+                </Button>    
+            </div>
 
         </div>
     
