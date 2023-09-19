22c7d5fd20f8a1d1423af91a14be40e2c6ad63e0
marty-m <m.miliauskas2@ncl.ac.uk>
2023-09-19 22:00:43 +0100

    BROKEN - Start building checkout page

--- app/cart/checkout/page.tsx
+++ app/cart/checkout/page.tsx
@@ -1,5 +1,131 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+
+ 
+const formSchema = z.object({
+    firstName: z.string().min(2).max(50),
+    lastName: z.string().min(2).max(50),
+    email: z.string().email(),
+    country: z.string().min(2).max(50),
+    address: z.string().min(2).max(50),
+    city: z.string().min(2).max(50),
+    zip: z.string().min(2).max(50),
+    phone: z.string().min(2).max(50),
+})
+
 export default function CheckoutPage() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            country: "",
+            address: "",
+            city: "",
+            zip: "",
+            phone: "",
+        },
+      })
+    
+    function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+    
     return (
-        <p>Checkout</p>
+        <div className="h-full grid grid-cols-2 divide-x divide-black items-start">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className=" overflow-auto grid grid-cols-2 gap-8 py-5 px-16">
+                    <FormField
+                    control={form.control}
+                    name="firstName"
+                    render={({ field }) => (
+                        <FormItem className="col-span-1">
+                        <FormLabel>First Name</FormLabel>
+                        <FormControl>
+                            <Input placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name="lastName"
+                    render={({ field }) => (
+                        <FormItem className="col-span-1">
+                        <FormLabel>Last Name</FormLabel>
+                        <FormControl>
+                            <Input placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem className="col-span-1">
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                            <Input type="email" placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name="country"
+                    render={({ field }) => (
+                        <FormItem className="col-span-1">
+                            <FormLabel>Country</FormLabel>
+                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                <FormControl>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select a verified email to display" />
+                                </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                <SelectItem value="m@example.com">m@example.com</SelectItem>
+                                <SelectItem value="m@google.com">m@google.com</SelectItem>
+                                <SelectItem value="m@support.com">m@support.com</SelectItem>
+                                </SelectContent>
+                            </Select>
+                            </FormItem>
+                            )}
+                            />
+
+                    <Button type="submit">Submit</Button>
+                
+                </form>
+            </Form>
+            <div className="overflow-auto flex flex-col h-full p-5">
+                <h1>Hio</h1>
+            </div>
+        </div>
     )
 }
