98728b453d59299014a25bf04f903480ac84cdb0
marty-m <m.miliauskas2@ncl.ac.uk>
2023-09-21 14:35:56 +0100

    Finish first stage checkout front end

--- app/cart/checkout/page.tsx
+++ app/cart/checkout/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import shipping from "@/lib/shipping.json"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
@@ -13,6 +14,8 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Separator } from "@/components/ui/separator"
 import {
     Select,
     SelectContent,
@@ -31,9 +34,12 @@ const formSchema = z.object({
     city: z.string().min(2).max(50),
     zip: z.string().min(2).max(50),
     phone: z.string().min(2).max(50),
+    mailingList: z.boolean(),
 })
 
 export default function CheckoutPage() {
+    const {countries} = shipping;
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -45,6 +51,7 @@ export default function CheckoutPage() {
             city: "",
             zip: "",
             phone: "",
+            mailingList: false,
         },
       })
     
@@ -57,7 +64,8 @@ export default function CheckoutPage() {
     return (
         <div className="h-full grid grid-cols-2 divide-x divide-black items-start">
             <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className=" overflow-auto grid grid-cols-2 gap-8 py-5 px-16">
+                <form onSubmit={form.handleSubmit(onSubmit)} className=" overflow-auto grid grid-cols-2 gap-4 py-5 px-16">
+                <div className="col-span-2 font-bold underline">Customer Details</div>
                     <FormField
                     control={form.control}
                     name="firstName"
@@ -65,7 +73,7 @@ export default function CheckoutPage() {
                         <FormItem className="col-span-1">
                         <FormLabel>First Name</FormLabel>
                         <FormControl>
-                            <Input placeholder="" {...field} />
+                            <Input className="border border-black rounded-none" placeholder="" {...field} />
                         </FormControl>
                         <FormMessage />
                         </FormItem>
@@ -78,7 +86,7 @@ export default function CheckoutPage() {
                         <FormItem className="col-span-1">
                         <FormLabel>Last Name</FormLabel>
                         <FormControl>
-                            <Input placeholder="" {...field} />
+                            <Input className="border border-black rounded-none" placeholder="" {...field} />
                         </FormControl>
                         <FormMessage />
                         </FormItem>
@@ -91,7 +99,7 @@ export default function CheckoutPage() {
                         <FormItem className="col-span-1">
                         <FormLabel>Email</FormLabel>
                         <FormControl>
-                            <Input type="email" placeholder="" {...field} />
+                            <Input className="border border-black rounded-none" type="email" placeholder="" {...field} />
                         </FormControl>
                         <FormMessage />
                         </FormItem>
@@ -99,32 +107,116 @@ export default function CheckoutPage() {
                     />
                     <FormField
                     control={form.control}
-                    name="country"
+                    name="phone"
                     render={({ field }) => (
                         <FormItem className="col-span-1">
-                            <FormLabel>Country</FormLabel>
-                            <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                <FormControl>
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Select a verified email to display" />
-                                </SelectTrigger>
-                                </FormControl>
-                                <SelectContent>
-                                <SelectItem value="m@example.com">m@example.com</SelectItem>
-                                <SelectItem value="m@google.com">m@google.com</SelectItem>
-                                <SelectItem value="m@support.com">m@support.com</SelectItem>
-                                </SelectContent>
-                            </Select>
+                        <FormLabel>Phone Number (with country code)</FormLabel>
+                        <FormControl>
+                            <Input className="border border-black rounded-none" placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    
+                    <Separator className="col-span-2" />
+                    <div className="col-span-2 font-bold underline">Delivery Address</div>   
+                    
+                    <FormField
+                    control={form.control}
+                    name="address"
+                    render={({ field }) => (
+                        <FormItem className="col-span-2">
+                        <FormLabel>Address</FormLabel>
+                        <FormControl>
+                            <Input className="border border-black rounded-none" placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    
+                    <FormField
+                    control={form.control}
+                    name="city"
+                    render={({ field }) => (
+                        <FormItem className="col-span-1">
+                        <FormLabel>City</FormLabel>
+                        <FormControl>
+                            <Input className="border border-black rounded-none" placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />     
+                    
+                    <FormField
+                    control={form.control}
+                    name="zip"
+                    render={({ field }) => (
+                        <FormItem className="col-span-1">
+                        <FormLabel>Zip Code</FormLabel>
+                        <FormControl>
+                            <Input className="border border-black rounded-none" placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="country"
+                        render={({ field }) => (
+                            <FormItem className="col-span-1">
+                                <FormLabel>Country</FormLabel>
+                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                    <FormControl>
+                                    <SelectTrigger className="border border-black rounded-none">
+                                        <SelectValue placeholder="" />
+                                    </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent className="border border-black rounded-none h-44 overflow-y-auto">
+                                        {countries.map((country) => (
+                                            <SelectItem key={country} value={country}>
+                                                {country}
+                                            </SelectItem>
+                                        ))}
+                                    
+                                    </SelectContent>
+                                </Select>
                             </FormItem>
                             )}
                             />
 
-                    <Button type="submit">Submit</Button>
+                    <FormField
+                        control={form.control}
+                        name="mailingList"
+                        render={({ field }) => (
+                            <FormItem className="flex flex-row items-center space-x-3 space-y-6 justify-center pt-1">
+                            <FormControl>
+                                <Checkbox
+                                className="rounded-none "
+                                checked={field.value}
+                                onCheckedChange={field.onChange}
+                                />
+                            </FormControl>
+                            <div className="space-y-1 leading-none">
+                                <FormLabel>
+                                I agree to receive information, special offers and promotions from UTC+1 clothing. 
+                                </FormLabel>
+                            </div>
+                            </FormItem>
+                        )}
+                        />
+                    <Button className="rounded-none col-span-2 mt-5" type="submit">To Shipping</Button>
                 
                 </form>
             </Form>
+
+
             <div className="overflow-auto flex flex-col h-full p-5">
-                <h1>Hio</h1>
+                <h1>Orders</h1>
             </div>
         </div>
     )
